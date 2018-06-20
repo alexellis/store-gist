fafe674f6f742a65971acb29eddb32599a52e4b1
Alex Ellis <alexellis2@gmail.com>
2018-06-20 22:39:46 +0100

    Create gist with token
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- store-gist/handler.go
+++ store-gist/handler.go
@@ -1,10 +1,55 @@
 package function
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 )
 
 // Handle a serverless request
-func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+func Handle(payload []byte) string {
+	url := "https://api.github.com/gists"
+
+	var jsonStr = []byte(`{
+                "description": ` + fmt.Sprintf("A gist of %d bytes", len(payload)) + `,
+                "public": true,
+                "files": {
+                        "file1.txt": {
+                            "content": "` + string(payload) + `"
+                        }
+                    }
+                }`)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Authorization", "token "+readSecret()) // The token
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.Status == "201 Created" {
+		return resp.Header.Get("Location")
+	}
+
+	fmt.Fprintf(os.Stderr, "Couldn't create file")
+	os.Exit(1)
+
+	return ""
+}
+
+func readSecret() string {
+	val, err := ioutil.ReadFile("/var/openfaas/secrets/github-token")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	return strings.TrimSpace(string(val))
 }
